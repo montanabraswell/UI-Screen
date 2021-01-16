3658d2a968078782cd6798789063d394214a73bc
Montana <montana@Montanas-MacBook-Pro.local>
2021-01-16 15:45:22 -0500

    Quick City UI screen

---
+++ AppTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTwo
+//
+//  Created by Montana  on 1/16/21.
+//  Copyright © 2021 Montana Braswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
